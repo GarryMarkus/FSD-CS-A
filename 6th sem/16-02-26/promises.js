12c51ce294a0ec41678aa02c66138434abd365da
garrymarkus <premsinghiitjee@gmail.com>
2026-02-16 10:18:26 +0530

    16-02-26 done

---
+++ 6th sem/16-02-26/promises.js
@@ -0,0 +1,20 @@
+const myPromise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        const success= Math.random() > 0.5;
+
+        if(success){
+            resolve("Success");
+        }
+        else{
+            reject("Error");
+        }
+    });
+});
+
+myPromise
+    .then((result)=>{
+        console.log(result);
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
